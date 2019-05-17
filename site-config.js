61570f6979972c532d231c9274ce7d9ba1d4847f
Carlos Felix-Beltran <felixbec@gmatc.matc.edu>
2019-05-17 11:16:50 -0500

    REFACTOR added content

--- site-config.js
+++ site-config.js
@@ -1,101 +1,63 @@
 module.exports = {
-  // App Info
-  appstore_link         : "https://imedadel.me",                                  // Enter App Store URL.
-  playstore_link        : "https://imedadel.me",                                  // Enter Google Play Store URL.
-  google_analytics_ID   : "UA-47311644-5",                                        // Enter Google Analytics ID or ""
-  presskit_download_link: "https://imedadel.me",                                  // Enter a link to downloadable file or (e.g. public Dropbox link to a .zip file). 
-  video_or_screenshot   : "screenshot",                                           // "screenshot" or "video"
-  app_url               : "https://agalp.imedadel.me",                            // Domain of your website without path_prefix.
-  path_prefix           : "/",                                                    // Prefixes all links. For cases when deployed to example.github.io/automatic-gatsbyjs-app-landing-page/.
-  app_name              : "AGALP",
-  app_price             : "Free",
-  app_description       : "Automatically generate GatsbyJS app landing pages.",
-  app_keywords          : ["automatic", "gatsbyjs", "app", "landing page"],
+  appstore_link: "empty",
+  playstore_link: "empty",
+  google_analytics_ID: "",
+  presskit_download_link: "",
+  video_or_screenshot: "video",
+  app_url: "http://au-designs.com",
+  path_prefix: "/",
+  app_name: "ARulity",
+  app_price: "Free",
+  app_description:
+    "Turn your device into measuring tool using Augmented Reality (AR) technology. You can accurately determine the sizes of objects using this technology. Measuring from Point A to Point B",
+  app_keywords: [
+    "automatic",
+    "gatsbyjs",
+    "app",
+    "landing page",
+    "AR",
+    "measure",
+    "Ios",
+  ],
 
-  // Personal Info
-  your_name              : "Imed Adel",
-  your_link              : "https://imedadel.me",
-  your_city              : "Sousse",
-  email_address          : "hello@imedadel.me",
-  facebook_username      : null,
-  instagram_username     : "ed_adel",
-  twitter_username       : "Imed_Adel",
-  github_username        : "ImedAdel",
-  youtube_username       : null,
+  your_name: "Carlos Felix-Beltran",
+  your_link: "http;//www.au-designs.com",
+  your_city: null,
+  email_address: null,
+  facebook_username: "UA-Designs",
+  instagram_username: "UA-Designs",
+  twitter_username: "UA-Designs",
+  github_username: null,
+  youtube_username: null,
 
-  // Features List
-  features               : [
+  features: [
     {
-      title: "GitHub Pages GatsbyJS Starter",
+      title: "Single Measurement",
       description:
-        "Designed for GitHub Pages and Netlify. Fork. Edit site-config.js. Upload screenshot/video. Push to gh-pages branch. Voilà!",
+        "ARulity technology allows users to select 2 points on the multi touch devices and retrieve accurate data measurement. Simply Magic!",
       fontawesome_icon_name: "magic",
-    },
-    {
-      title: "iPhone Device Preview",
-      description:
-        "Preview your app in the context of an iPhone device. Five different device colors included.",
-      fontawesome_icon_name: "mobile",
-    },
-    {
-      title: "Video Support",
-      description:
-        "Preview app video on the iPhone device simply by placing your video files in the videos folder.",
-      fontawesome_icon_name: "play-circle",
-    },
-    {
-      title: "Optimized Performance",
-      description:
-        "No need to worry about page performance or optimizing images and videos. Everything is already configured for the best performance possible!",
-      fontawesome_icon_name: "sync",
-    },
-    {
-      title: "Easy to Tweak",
-      description:
-        "Tweak accent color, images, icons and transparency via the site-config.js file. No HTML/CSS needed.",
-      fontawesome_icon_name: "adjust",
-    },
-    {
-      title: "Feature List",
-      description:
-        "Add features (like this one) to your site via the site-config.js file. No HTML/CSS needed.",
-      fontawesome_icon_name: "star",
-    },
-    {
-      title: "Deploy to Netlify",
-      description: "Easily deploy your GatsbyJS landing page to Netlify.",
-      fontawesome_icon_name: "arrow-alt-circle-down",
-    },
-    {
-      title: "Social Links",
-      description:
-        "Easily add social media accounts and contact info in the footer via the site-config.js file. No HTML/CSS needed.",
-      fontawesome_icon_name: "link",
-    },
-    {
-      title: "FontAwesome Support",
-      description:
-        "Pick custom Font Awesome icons for the feature list via the site-config.js file. No HTML/CSS needed.",
-      fontawesome_icon_name: "info-circle",
+      title2: "Multiple Metrics Support",
+      description2:
+        "Preview the data in multiple measuring metrics such as Inches & Centimeters. ",
+      fontawesome_icon_name2: "mobile",
     },
   ],
-  header_background             : "rgba(0, 0, 0, 0.1)",
-  topbar_title_color            : "#ffffff",
-  cover_overlay_color_rgba      : "rgba(54, 59, 61, 0.8)",
-  device_color                  : "black",                          // Set to "black", "blue", "coral", "white", or "yellow"
-  body_background_color         : "ffffff",
-  primary_text_color            : "#000",
-  content_width                 : "1170px",
-  font                          : `"Helvetica Neue", sans-serif`,
-  link_color                    : "#1d63ea",
-  app_title_color               : "#ffffff",
-  app_price_color               : "#ffffff",
-  app_description_color         : "#ffffff",
-  feature_title_color           : "#000000",
-  feature_text_color            : "#666666",
+  header_background: "#fff",
+  topbar_title_color: "#ffffff",
+  cover_overlay_color_rgba: "rgba(54, 59, 61, 0.8)",
+  device_color: "white",
+  body_background_color: "#ffffff",
+  primary_text_color: "#000",
+  content_width: "1170px",
+  font: `"Helvetica Neue", sans-serif`,
+  app_title_color: "#ffffff",
+  app_price_color: "#ffffff",
+  app_description_color: "#ffffff",
+  feature_title_color: "#000000",
+  feature_text_color: "#666666",
   feature_icons_foreground_color: "#1d63ea",
   feature_icons_background_color: "#e6e6e6",
-  social_icons_foreground_color : "#666666",
-  social_icons_background_color : "#e6e6e6",
-  footer_text_color             : "#666666",
+  social_icons_foreground_color: "#666666",
+  social_icons_background_color: "#e6e6e6",
+  footer_text_color: "#666666",
 }
